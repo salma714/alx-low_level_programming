00d51fdc1e428d8130c1001a0509e76a646d83d1
salma <salma@gmail.com>
2023-03-20 14:28:42 -0700

    prrint

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,18 +12,21 @@ int main(void)
 {
 	int d, p, s;
 
-	for (d = '0'; d < '9'; d++)
+	for (d = 0; d < 10; d++)
 	{
-		for (p = d + 1; p < '9'; p++)
+		for (p = d + 1; p < 10; p++)
 		{
-			for (s = p = 1; s < '9'; s++)
+			for (s = p + 2; s < 10; s++)
 			{
 				if (d < p && p < s)
 				putchar(d + '0');
 				putchar(p + '0');
 				putchar(s + '0');
-				putchar(',');
-				putchar(' ');
+				if (d + p + s != 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
