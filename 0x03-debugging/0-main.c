bbdd9fbba9aa3fecaf53317445648424794c40bc
salma <salma@gmail.com>
2023-03-24 19:29:57 -0700

    psitive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test fonction for positive or negative
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
